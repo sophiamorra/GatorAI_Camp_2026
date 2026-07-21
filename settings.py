d91adb7153965902cc76d95b4a48a887853562fe
sophiamorra <sophia_morra@mynbps.org>
2026-07-21 11:17:06 -0400

    change

--- settings.py
+++ settings.py
@@ -175,7 +175,7 @@ NPC_DATA = {
         ]  # Default starter dialogue lines
     },
     "Jen": {
-        "pos": (700, 300),  # Default spawn position
+        "pos": (720, 320),  # Default spawn position
         "graphic": "graphics/objects/merchant.png",  # Placeholder using existing asset (Default: "graphics/objects/merchant.png")
         "dialogue": [
             "Hi there! Welcome to Sophia's Island!",
